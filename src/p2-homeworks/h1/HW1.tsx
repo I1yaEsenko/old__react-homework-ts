6fb3e15829bfdc6bfd4bfd4749a24b8810e4c937
Ilya Esenko <88851125+I1yaEsenko@users.noreply.github.com>
2021-12-09 00:54:16 +0300

    Add HW1 , update html and css, add propstype

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Message from "./Message";
+
+
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
-    message: 'some text',
+    message: 'some textome texome texome texome texome texome texome tex',
     time: '22:00',
 }
 
@@ -15,12 +18,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import m from './Message.module.css'
 
-function Message() {
-    return (
-        <div>
+type MessagePropsType = {
+  avatar: string
+  name: string
+  message: string
+  time: string
+}
 
+function Message(props: MessagePropsType) {
+  return (
+    <div className={m.message}>
+      <div className={m.img}><img src={props.avatar} alt=""/></div>
+      <div className={m.wrapper}>
+        <div className={m.item}>
+          <div className={m.title}>{props.name}</div>
+          <div className={m.text}>{props.message}</div>
         </div>
-    )
+        <div className={m.date}>{props.time}</div>
+      </div>
+      <svg height="0" width="0">
+        <defs>
+          <clipPath id="svgPath">
+            <path fill="#FFFFFF" d="M10,10H0V0H0A10,10,0,0,0,10,10Z"/>
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  )
 }
 
 export default Message
